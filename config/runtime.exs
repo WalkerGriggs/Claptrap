0b3d4d6ca76c82d2e9487f2f612a5ad36de03e61
WalkerGriggs <walker@walkergriggs.com>
2026-03-28 14:40:15 -0700

    chore(config): split apart db environment variables

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,9 @@
 import Config
 
+must_get = fn name ->
+  System.get_env(name) || raise "#{name} environment variable is not set"
+end
+
 if db_host = System.get_env("DATABASE_HOSTNAME") do
   config :claptrap, Claptrap.Repo, hostname: db_host
 end
@@ -13,21 +17,27 @@ if config_env() == :test do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise "DATABASE_URL environment variable is not set"
+  db_host = must_get.("DATABASE_HOST")
 
   config :claptrap, Claptrap.Repo,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    database: must_get.("DATABASE"),
+    hostname: db_host,
+    port: String.to_integer(System.get_env("DATABASE_PORT") || "5432"),
+    username: must_get.("DATABASE_USERNAME"),
+    password: must_get.("DATABASE_PASSWORD"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    ssl: [
+      verify: :verify_peer,
+      cacertfile: CAStore.file_path(),
+      server_name_indication: String.to_charlist(db_host),
+      customize_hostname_check: [
+        match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+      ]
+    ]
 
-  api_key =
-    System.get_env("CLAPTRAP_API_KEY") ||
-      raise "CLAPTRAP_API_KEY environment variable is not set"
-
-  config :claptrap, api_key: api_key
+  config :claptrap, api_key: must_get.("CLAPTRAP_API_KEY")
 
   config :claptrap, :firecrawl,
-    api_key: System.fetch_env!("FIRECRAWL_API_KEY"),
+    api_key: must_get.("FIRECRAWL_API_KEY"),
     base_url: System.get_env("FIRECRAWL_BASE_URL", "https://api.firecrawl.dev")
 end
